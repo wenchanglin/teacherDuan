5c3664543d9fe963faead2c7bf5e37555092c377
LiHui <xiaoxiaoh99@163.com>
2016-09-18 09:07:53 +0800

    Initial Commit

---
+++ ShangKTeacher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShangKTeacher
+//
+//  Created by apple on 16/9/18.
+//  Copyright © 2016年 Fbw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShangKTeacher/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShangKTeacher
+//
+//  Created by apple on 16/9/18.
+//  Copyright © 2016年 Fbw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
